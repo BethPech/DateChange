72c985ec2527abeab19d5ae01ba080bf45faa6d3
BethPech <colnze17-94@mail.ru>
2021-02-06 13:49:52 +0300

    Tests deleted

--- src/test/java/ru/netology/web/DateChangeTest.java
+++ src/test/java/ru/netology/web/DateChangeTest.java
@@ -13,35 +13,6 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DateChangeTest {
-    Date date = new Date();
-    String str = String.format("%tc", date);
 
-    @Test
-    void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Казань");
-        $("[placeholder='Дата встречи']").val(str);
-        $("[name='name']").setValue("Петров Иван");
-        $("[name='phone']").setValue("+79000000000");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
-    }
-
-    @Test
-    void shouldFailTestBecauseOfLowCaseLetters() {
-        open("http://localhost:9999");
-        $$("input__control");
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").val(str);
-        $("[name='name']").setValue("ванов-етров авел");
-        $("[name='phone']").setValue("+98765432145");
-        $("[class = 'checkbox__box']").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(exist);
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
-    }
 
 }
